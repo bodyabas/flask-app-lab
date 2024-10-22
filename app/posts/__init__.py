ec3f637aad436d7cf51cc4fcc6abaec1579b02cb
Bohdan Basiuryn <bohbanbas@gmail.com>
2024-10-22 10:25:14 +0300

    added blueprint with posts

---
+++ app/posts/__init__.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+post_bp = Blueprint("posts", __name__, url_prefix="/post", template_folder="templates")
+
+from . import views
--- app/views.py
+++ app/views.py
@@ -24,22 +24,6 @@ def admin():
     print(to_url)
     return redirect(to_url)
 
-posts = [
-    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
-    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
-    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-] 
-
-@app.route("/posts") 
-def get_posts():
-    return render_template("posts.html", posts = posts)
-
-@app.route("/post/<int:id>") 
-def detail_post(id):
-    if id > 3:
-        abort(404)
-    post = posts[id-1]
-    return render_template("detail_post.html", post=post)
 
 @app.route("/resume")
 def resume():
