b4ed2a5700c39e79d7a60676f98704c280764c58
Bohdan Basiuryn <bohbanbas@gmail.com>
2024-10-22 09:39:55 +0300

    app as module added

---
+++ app/views.py
@@ -0,0 +1,46 @@
+from flask import request, redirect, url_for, render_template, abort
+from . import app
+
+@app.route("/")
+def main():
+    return render_template("base.html")
+
+@app.route("/homepage") 
+def home():
+    """View for the Home page of your website."""
+    agent = request.user_agent
+    return render_template("home.html", agent = agent)
+
+@app.route("/hi/<string:name>")
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", 0, int)
+
+    return render_template("hi.html", name = name, age = age)
+
+@app.route("/admin")
+def admin():
+    to_url = url_for("greetings", name="administrator", age=45, _external=True)
+    print(to_url)
+    return redirect(to_url)
+
+posts = [
+    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+] 
+
+@app.route("/posts") 
+def get_posts():
+    return render_template("posts.html", posts = posts)
+
+@app.route("/post/<int:id>") 
+def detail_post(id):
+    if id > 3:
+        abort(404)
+    post = posts[id-1]
+    return render_template("detail_post.html", post=post)
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
