bb159f1319f28eba2a072cbbc323019628f3a699
Bohdan Basiuryn <bohbanbas@gmail.com>
2024-10-27 21:00:15 +0200

    added and registered blueprint for users

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,7 @@ app.config.from_pyfile("../config.py")
 from . import views
 
 from .posts import post_bp
+from .users import user_bp
+
 app.register_blueprint(post_bp)
+app.register_blueprint(user_bp)
--- app/views.py
+++ app/views.py
@@ -11,20 +11,6 @@ def home():
     agent = request.user_agent
     return render_template("home.html", agent = agent)
 
-@app.route("/hi/<string:name>")
-def greetings(name):
-    name = name.upper()
-    age = request.args.get("age", 0, int)
-
-    return render_template("hi.html", name = name, age = age)
-
-@app.route("/admin")
-def admin():
-    to_url = url_for("greetings", name="administrator", age=45, _external=True)
-    print(to_url)
-    return redirect(to_url)
-
-
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
