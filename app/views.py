8b94373f24c43b1957656944882a007ef16c3811
Bohdan Basiuryn <bohbanbas@gmail.com>
2024-11-10 13:26:10 +0200

    added 404 page and added styles to it

--- app/views.py
+++ app/views.py
@@ -14,3 +14,8 @@ def home():
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # Відображаємо шаблон 404.html і повертаємо статусний код 404
+    return render_template('404.html'), 404
